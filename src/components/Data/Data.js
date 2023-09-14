0d09fa72623b914eb3be1a78bbece1b7f417a988
Jaron <Jaronleach98@gmail.com>
2023-09-14 13:08:20 -0400

    Created a init datatable to be used for my music table and searchbar.

---
+++ src/components/Data/Data.js
@@ -0,0 +1,74 @@
+const initData = [
+  {
+    song: "Backseat FreeStyle",
+    album: "Good kid m.A.Ad city",
+    artist: "Kendrick Lamar",
+    genre: "Rap",
+    releaseData: 10 / 22 / 12,
+  },
+  {
+    song: "Marvins Room",
+    album: "Take Care",
+    artist: "Drake",
+    genre: "R&B",
+    releaseData: 6 / 28 / 11,
+  },
+  {
+    song: "She",
+    album: "Goblin",
+    artist: "Tyler The Creator",
+    genre: "Rap",
+    releaseData: 5 / 10 / 11,
+  },
+  {
+    song: "Oldie",
+    album: "Odd Future",
+    artist: "Odd Future",
+    genre: "Rap",
+    releaseData: 3 / 20 / 13,
+  },
+  {
+    song: "Wet Dreamz",
+    album: "Forrest Hill Drives",
+    artist: "J Cole",
+    genre: "Rap",
+    releaseData: 12 / 9 / 14,
+  },
+  {
+    song: "Otis",
+    album: "Watch The Throne",
+    artist: "Jay Z",
+    genre: "Rap",
+    releaseData: 8 / 8 / 11,
+  },
+  {
+    song: "All of the Lights",
+    album: "My Beautifly Dark Twisted Fantasy",
+    artist: "Kayne West",
+    genre: "Rap",
+    releaseData: 11 / 20 / 20,
+  },
+  {
+    song: "Crazy Story ",
+    album: "Grandson",
+    artist: "King VOn",
+    genre: "Rap",
+    releaseData: 12 / 6 / 19,
+  },
+  {
+    song: "Sum 2 Prove",
+    album: "My Turn",
+    artist: "Lil Bbay",
+    genre: "Rap",
+    releaseData: 2 / 28 / 20,
+  },
+
+  {
+    song: "What Happened to Virgil",
+    album: "7220",
+    artist: "Jay Z",
+    genre: "Rap",
+    releaseData: 3 / 11 / 22,
+  },
+];
+export default initData;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+
+const searchbar = () => {};
+const [searchInput, setSearchInput] = useState("");
+
+const Music = [];
